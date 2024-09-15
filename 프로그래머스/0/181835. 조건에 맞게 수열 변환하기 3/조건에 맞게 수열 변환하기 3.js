4be0ac8a45ed5e1ea7c8b4d7c6a383f2f204a014
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-15 16:08:41 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 17.33 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,3 +1,12 @@
 function solution(arr, k) {
     return arr.map(num => k % 2 === 1 ? num * k : num + k);
+    // const result =arr.map(list => {
+    //     if(k % 2 === 1){
+    //         list = list * k;
+    //     }else if(k % 2 === 0){
+    //         list = list + k;
+    //     }
+    //     return list;
+    // })
+    // return result;
 }
