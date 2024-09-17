e59a0d9897c4ca725c579ffeefcc8d15f75f4fc4
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-17 12:38:56 +0900

    [level 0] Title: l로 만들기, Time: 10.34 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,3 +1,7 @@
 function solution(myString) {
-    return myString.replace(/[a-k]/g,'l')
+    let result = '';
+    for (let i = 0; i < myString.length; i++) {
+        result += myString[i] < 'l' ? 'l' : myString[i];
+    }
+    return result;
 }
