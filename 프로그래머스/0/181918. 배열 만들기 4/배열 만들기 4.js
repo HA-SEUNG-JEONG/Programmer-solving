6be5707244679925c634d65f133ae5a84f1cb6bc
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-18 17:21:38 +0900

    [level 0] Title: 배열 만들기 4, Time: 21.46 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,20 +1,5 @@
 function solution(arr) {
-    let stk = [];
-    // for(let i=0; i<arr.length;i++){
-    //     if(arr.length === 0){
-    //         stk+=arr[i];
-    //         i+=1;
-    //     }
-    //     else if(arr.length !== 0 && stk.at(-1) < arr[i]){
-    //         stk.push(arr[i]);
-    //         i+=1;
-    //     }
-    //     else if(stk.length !== 0 && stk.at(-1)>=arr[i]){
-    //         stk.pop()
-    //     }
-    // }
-    // return stk
-    
+    const stk = [];
     let i = 0;
     
     while(i < arr.length){
