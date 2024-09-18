c58a8c573266dc4e552966bdd569797f0ff4d27f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-18 15:45:42 +0900

    [level 0] Title: 수 조작하기 2, Time: 236.45 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,24 +1,16 @@
 function solution(numLog) {
-    let result = '';
+    const diffToChar = {
+        1: 'w',
+        '-1': 's',
+        10: 'd',
+        '-10': 'a'
+    };
     
-    for(let i=1;i<numLog.length;i++){
-        const diff = numLog[i] - numLog[i - 1];
-        
-        switch(diff){
-             case 1:
-                result += 'w';
-                break;
-            case -1:
-                result += 's';
-                break;
-            case 10:
-                result += 'd';
-                break;
-            case -10:
-                result += 'a';
-                break;   
-        }
+    const result = new Array(numLog.length - 1);
+    
+    for (let i = 1; i < numLog.length; i++) {
+        result[i-1] = diffToChar[numLog[i] - numLog[i-1]];
     }
     
-    return result;
+    return result.join('');
 }
