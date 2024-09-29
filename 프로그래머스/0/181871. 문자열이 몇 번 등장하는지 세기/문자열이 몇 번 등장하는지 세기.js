24812a7f4ab3784b8e191af4244b6294bc32de65
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-09-29 09:48:49 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,10 @@
+function solution(myString, pat) {
+    let count = 0;
+    const patLength = pat.length;
+
+    for (let i = 0; i <= myString.length - patLength; i++) {
+        if (myString.slice(i, i + patLength) === pat) count++;
+    }
+
+    return count;
+}
