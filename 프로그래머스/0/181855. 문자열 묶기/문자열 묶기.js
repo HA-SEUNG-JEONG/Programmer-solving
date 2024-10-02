be16a5bf7f81958cebf2d34f2e2190c1cb7ca3e7
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-10-02 15:55:33 +0900

    [level 0] Title: 문자열 묶기, Time: 4.26 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,14 +1,11 @@
 function solution(strArr) {
-    const lengthMap = new Map();
-
-    strArr.forEach(str => {
-        const len = str.length;
-        if (lengthMap.has(len)) {
-            lengthMap.set(len, lengthMap.get(len) + 1);
-        } else {
-            lengthMap.set(len, 1);
-        }
+    const lengthCount = {};
+    
+    strArr.forEach((str) => {
+        const length = str.length;
+        if(lengthCount[length]) lengthCount[length]++;
+        else lengthCount[length] = 1;
     });
-
-    return Math.max(...lengthMap.values());
+    
+    return Math.max(...Object.values(lengthCount));
 }
