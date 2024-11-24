c7ef5aaeee5e3d71409abcd6c55faf8236bc73e7
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-11-24 13:37:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,20 +1,20 @@
 function solution(s) {
-    const numbers = {
-        'zero' : 0,
-        'one' : 1,
-        'two' : 2,
-        'three' : 3,
-        'four' : 4,
-        'five' : 5,
-        'six' : 6,
-        'seven' : 7,
-        'eight' : 8,
-        'nine' : 9
-    }
+    const numbers = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine'
+    ]
     
     let result = s;
-    for (let [word, digit] of Object.entries(numbers)) {
-        result = result.replace(new RegExp(word, 'g'), digit);
+    for (let i=0; i<numbers.length; i++) {
+        result = result.split(numbers[i]).join(i);
     }
     
     return Number(result);
