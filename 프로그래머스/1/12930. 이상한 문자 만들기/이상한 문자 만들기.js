1163f64dfa6949a96b3ab0f4c84b8737d59eb196
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-04 09:50:38 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,7 @@
 function solution(s) {
-    let result = '';
-    let index = 0;
-    
-    for(let i=0; i<s.length; i++){
-        if(s[i] === ' '){
-            result += ' ';
-            index = 0;
-            continue;
-        }
-        
-        if(index % 2 === 0){
-            result += s[i].toUpperCase();
-        }else{
-            result += s[i].toLowerCase();
-        }
-        index++;
-    }
-    return result;
+    return s.split(' ').map(word => {
+        return word.split('').map((char, index) => {
+            return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+        }).join('');
+    }).join(' ');
 }
