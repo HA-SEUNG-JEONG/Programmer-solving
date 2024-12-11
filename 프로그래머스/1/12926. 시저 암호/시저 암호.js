5ccdeb575186f63eeb2a4556049b2d46c9915c3c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-11 13:56:34 +0900

    [level 1] Title: 시저 암호, Time: 1.76 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,14 +1,9 @@
 function solution(s, n) {
-     const lower = 'abcdefghijklmnopqrstuvwxyz';
-     const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    
-     const shiftLower = lower.slice(n) + lower.slice(0,n);
-    const shiftUpper = upper.slice(n) + upper.slice(0,n);
-    
-  return s.split('').map(char => {
-      if(char === ' ') return char;
-      const alphabet = lower.includes(char) ? shiftLower : shiftUpper;
-      return alphabet[char.toLowerCase().charCodeAt(0)-97]
-  }).join('')
-    
+ return s.split('').map(char => {
+    if (char === ' ') return char;
+    const code = char.charCodeAt(0);
+    const isUpperCase = code >= 65 && code <= 90;
+    const base = isUpperCase ? 65 : 97;
+    return String.fromCharCode((code - base + n) % 26 + base);
+  }).join('');
 }
