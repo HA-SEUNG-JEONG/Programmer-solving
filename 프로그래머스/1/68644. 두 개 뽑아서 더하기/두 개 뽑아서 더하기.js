4732817c94b321aad5871c3f8f1d1ab3e4a4ae3d
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-12 12:04:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const result = new Set();
+    const length = numbers.length;
+    
+    let sum = 0;
+    
+    for(let i=0; i<length-1;i++){
+        for(let j=i+1;j<length;j++){
+            result.add(numbers[i]+numbers[j])
+        }
+    }
+    return Array.from(result).sort((a,b) => a-b);
+}
