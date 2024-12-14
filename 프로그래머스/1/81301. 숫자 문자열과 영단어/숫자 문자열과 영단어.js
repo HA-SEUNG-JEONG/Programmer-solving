1db4c7c572c455bd559e4e31dc01d23954a2e24b
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-14 10:31:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,21 +1,21 @@
 function solution(s) {
-    const numbers = [
-        'zero',
-        'one',
-        'two',
-        'three',
-        'four',
-        'five',
-        'six',
-        'seven',
-        'eight',
-        'nine'
-    ]
-    
-    let result = s;
-    for (let i=0; i<numbers.length; i++) {
-        result = result.split(numbers[i]).join(i);
-    }
-    
-    return Number(result);
+    const numWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        let result = '';
+        let temp = '';
+
+        for (let char of s) {
+            // 숫자면 그대로 추가
+            if (!isNaN(char)) {
+                result += char;
+            } else {
+                // temp가 numWords 배열에 있는 단어와 일치하면, 해당 단어의 인덱스(숫자 값)를 result에 추가하고 temp를 초기화
+                temp += char;
+                if (numWords.includes(temp)) {
+                    result += numWords.indexOf(temp);
+                    temp = '';
+                }
+            }
+        }
+
+        return Number(result);
 }
