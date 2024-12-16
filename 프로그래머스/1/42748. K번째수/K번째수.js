c8bc50912a78be848604cf7e8dc2a376ae2f96f8
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-16 10:07:59 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,7 @@
 function solution(array, commands) {
     return commands.map(([i,j,k]) => {
-        const slicedArray = array.slice(i-1,j).sort((a,b) => a-b)[k-1];
-        return slicedArray;
+        const slicedArray = array.slice(i-1,j);
+        const sortedArray = slicedArray.sort((a,b) => a-b);
+        return sortedArray[k-1];
     })
 }
