0706fcd9becd940703a3cc2ce66472c223b995aa
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-19 10:03:02 +0900

    [level 1] Title: 추억 점수, Time: 1.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,10 +1,15 @@
 function solution(name, yearning, photo) {
-    const scoreMap = new Map(name.map((n, i) => [n, yearning[i]]));
-    return photo.map(people => {
-        let score = 0;
-        for (let person of people) {
-            score += scoreMap.get(person) || 0;
-        }
-        return score;
-    });
-} 
+    const result = [];
+    // 그리워하는 사람의 이름을 담은 문자열 배열 name, 
+    // 각 사람별 그리움 점수를 담은 정수 배열 yearning, 
+    // 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo
+    const scoreMap = new Map();
+    name.forEach((element,index) => {
+        scoreMap.set(element,yearning[index]);
+    })
+    
+    return photo.map((persons) => 
+        persons.reduce((acc, person) => 
+            acc + (scoreMap.get(person) || 0), 0)
+    );
+}
