9dca28b5e02d4cc6d8d7a33ea7fef56c784de0c6
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-21 09:51:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 213.95 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,25 @@
+function divisor(num){
+    const answer = [];
+    for(let i = 1; i<= Math.sqrt(num); i++){
+        if(num % i == 0){
+            answer.push(i);
+            if( i * i != num){
+                answer.push(num / i);
+            }
+        }
+    }
+    answer.sort(function(a,b){
+        return a - b;
+    })
+    
+    return answer.length;
+}
+function solution(number, limit, power) {
+    let total = 0;
+    for(let i=1; i<=number; i++){
+        let divisorCount = divisor(i);
+        if (divisorCount > limit) total+=power;
+        else total += divisorCount;
+    }
+    return total;
+}
