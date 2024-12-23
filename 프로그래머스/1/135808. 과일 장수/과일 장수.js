15876651b036bff67bb834416178a1599eb6d65d
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2024-12-23 09:19:47 +0900

    [level 1] Title: 과일 장수, Time: 44.26 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,27 @@
 function solution(k, m, score) {
     // 사과의 최대 점수 k, 한 상자에 들어가는 사과의 수 m, 사과들의 점수 score
-    const sortedScore = score.sort((a,b) => b-a);
-    let maxProfit = 0;
-    for(let i = m-1; i < sortedScore.length; i += m) {
-        const lowestScore = sortedScore[i];
-        maxProfit += lowestScore * m;
+    const counts = new Array(k + 1).fill(0);
+    // 점수별 개수 카운팅
+    for (const s of score) {
+        counts[s]++;
     }
+    
+    let maxProfit = 0;
+    let currentBox = 0;
+    let remainingSlots = m;
+    
+    // 높은 점수부터 처리
+    for (let i = k; i >= 1; i--) {
+        while (counts[i] > 0) {
+            counts[i]--;
+            remainingSlots--;
+            
+            if (remainingSlots === 0) {
+                maxProfit += i * m;
+                remainingSlots = m;
+            }
+        }
+    }
+    
     return maxProfit;
 }
