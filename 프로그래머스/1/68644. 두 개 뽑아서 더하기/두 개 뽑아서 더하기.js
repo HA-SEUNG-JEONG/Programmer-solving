9cea9b485c1504add4063e9aced67a5517502af2
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-02 12:00:12 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.59 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,13 +1,10 @@
 function solution(numbers) {
-    const result = new Set();
-    const length = numbers.length;
-    
-    let sum = 0;
-    
-    for(let i=0; i<length-1;i++){
+    const set = new Set();
+    const length = numbers.length
+    for(let i=0;i<length-1;i++){
         for(let j=i+1;j<length;j++){
-            result.add(numbers[i]+numbers[j])
+            set.add(numbers[i]+numbers[j]);
         }
     }
-    return [...result].sort((a,b) => a-b);
+    return [...set].sort((a,b) => a-b)
 }
