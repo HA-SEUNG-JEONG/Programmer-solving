9e98b395a21846a46c63524fb32777ecd1118d78
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-02 10:35:58 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.91 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,12 +1,15 @@
 function solution(keymap, targets) {
     // 1번 키를 한번 누르면 A, 두번 누르면 B, 세번 누르면 C
     const minPress = new Map();
-    keymap.forEach(key => {
-        [...key].forEach((char,index) => {
-            const count = index+1;
-            if(!minPress.has(char) || minPress.get(char) > count) minPress.set(char,count);
-        })
-    });
+    for (const key of keymap) {
+        for (let i = 0; i < key.length; i++) {
+            const char = key[i];
+            const count = i + 1;
+            if (!minPress.has(char) || minPress.get(char) > count) {
+                minPress.set(char, count);
+            }
+        }
+    }
     
     return targets.map(target => {
         let total = 0;
