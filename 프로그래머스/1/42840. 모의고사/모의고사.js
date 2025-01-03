df04269ff8f915be42e093801fbaaf991cbe5f06
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-03 12:46:02 +0900

    [level 1] Title: 모의고사, Time: 16.76 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,35 @@
 function solution(answers) {
-    const patterns = [
+    const patterns =[
         [1,2,3,4,5],
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
     ];
     
-    const score = [0,0,0]
+    // 수포자들의 점수 저장할 배열
+    const result = [0,0,0];
     
-    for (let i = 0; i < answers.length; i++) {
-        const answer = answers[i];
-        for (let j = 0; j < patterns.length; j++) {
-            const pattern = patterns[j];
-            if (answer === pattern[i % pattern.length]) {
-                score[j]++;
+//     for(let i=0;i<answers.length-1;i++){
+//         // patterns[i]가 answers에 포함되어 있는지?
+//        for(let j=i;j<answers.length;j++){
+        
+//        }
+//     }
+    for(const [i,answer] of answers.entries()){
+        for(const [j,pattern] of patterns.entries()){
+            if(answer === pattern[i % pattern.length]){
+                result[j]++;
             }
         }
     }
     
-    const maxScore = Math.max(...score);
+    const maxScore = Math.max(...result)
     
-    return score.reduce((result,score,index) => {
-        if(score === maxScore) result.push(index+1);
-        return result
-    },[])
+    const highestScore = [];
+    for(let k=0;k<result.length;k++){
+        if(result[k] === maxScore){
+            highestScore.push(k+1);
+        }
+    }
+    
+    return highestScore
 }
