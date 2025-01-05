37a0c9d247980e9d04276fce2903da327d87aebb
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-05 17:19:03 +0900

    [level 1] Title: 실패율, Time: 6517.84 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,15 +1,16 @@
 function solution(N, stages) {
-    // 전체 스테이지 개수 : N, 게임을 이용하는 사용자가 멈춰있는 스테이지 번호가 담긴 배열 stages
-    // 실패율이 높은 스테이지부터 내림차순으로 스테이지 번호가 담겨있는 배열을 return
-    // 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록
-    // 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0
+    // 실패율 : 스테이지에 도달했으나 아직 클리어 못한 플레이어 수 / 스테이지에 도달한 플레이어 수 
+    // 전체 스테이지 개수 : N
+    // 사용자가 현재 멈춰있는 스테이지 번호가 담긴 배열 : stages
+    // 실패율이 높은 스테이지부터 내림차순으로 스테이지를 담은 배열을 반환
     
-    let result = [];
-    for(let i=1; i<=N; i++){
-        let reach = stages.filter((x) => x >= i).length;
-        let curr = stages.filter((x) => x === i).length;
-        result.push([i, curr/reach]);
+    const result = [];
+    for(let i=1;i<=N;i++){
+        // 전체 스테이지 수에서 클리어하지 못한 사람의 수만큼 빼기
+        const reach = stages.filter(stage => stage >= i).length;
+        const current = stages.filter(stage => stage === i).length;
+        result.push([i,current/reach]);
     }
-    result.sort((a,b) => b[1] - a[1]);
-    return result.map((x) => x[0]);
+    result.sort((a,b) => b[1]-a[1])
+    return result.map(x=>x[0])
 }
