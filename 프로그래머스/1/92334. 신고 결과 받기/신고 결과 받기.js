989b250167a518bd803e7531e87fa06875d31f37
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-14 11:17:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 225.47 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,40 @@
+function solution(id_list, report, k) {
+    // k번 이상 신고된 유저는 게시판 이용이 정지
+    // 유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송
+    // 이용자의 ID가 담긴 문자열 배열 id_list
+    // 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열 report
+    // 정지 기준이 되는 신고 횟수 k
+    // 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 리턴
+    
+    // report의 원소는 "이용자id 신고한id"형태의 문자열
+    
+    // 유저ID(key)와 신고횟수(value)를 담을 객체 필요
+    
+    const reportMap = new Map();
+    const mailCount = new Map()
+    
+    id_list.forEach(id => {
+        reportMap.set(id, new Set());
+        mailCount.set(id,0)
+    })
+    
+    report.forEach((rep) => {
+        const [userId, reportId] = rep.split(' '); // userId가 reportId를 신고
+        // 신고를 한 경우 해당 신고를 한 userId를 키로 담아서 value 카운트..
+
+        reportMap.get(reportId).add(userId)
+    });
+    
+    
+    reportMap.forEach((report,userId) => {
+        if(report.size >= k){
+            report.forEach(reporter => {
+                mailCount.set(reporter,mailCount.get(reporter)+1);
+            });
+        }
+    })
+    
+    return id_list.map(id => mailCount.get(id))
+    
+
+}
