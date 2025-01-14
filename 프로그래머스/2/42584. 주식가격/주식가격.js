1b8e7a978695156516283443b5409be3ca59bc48
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-14 13:24:21 +0900

    [level 2] Title: 주식가격, Time: 4.86 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,16 +1,33 @@
 function solution(prices) {
-    const stack = new Array(prices.length).fill(0);
-    let count = 0;
-    // 가격이 떨어지지 않은 기간은 몇 초인지 리턴
-    // 현재 인덱스 요소보다 다음 인덱스보다 같거나 큰 경우
-    for(let i=0;i<prices.length;i++){
-        // 몇 초인지 판단은 어떻게 판단?
-        // 인덱스끼리 마이너스 연산(ex. i 위치에 있는 1이 i+4까지 떨어지지 않았다면 (i+4) - i)
-        // console.log(prices.indexOf(prices[i],i))
-        for(let j=i+1;j<prices.length;j++){
-            stack[i]++;
-            if(prices[i] > prices[j]) break;
+    // const stack = new Array(prices.length).fill(0);
+    // let count = 0;
+    // // 가격이 떨어지지 않은 기간은 몇 초인지 리턴
+    // // 현재 인덱스 요소보다 다음 인덱스보다 같거나 큰 경우
+    // for(let i=0;i<prices.length;i++){
+    //     // 몇 초인지 판단은 어떻게 판단?
+    //     // 인덱스끼리 마이너스 연산(ex. i 위치에 있는 1이 i+4까지 떨어지지 않았다면 (i+4) - i)
+    //     // console.log(prices.indexOf(prices[i],i))
+    //     for(let j=i+1;j<prices.length;j++){
+    //         stack[i]++;
+    //         if(prices[i] > prices[j]) break;
+    //     }
+    // }
+    // return stack;
+    const n = prices.length;
+    const result = new Array(n).fill(0);
+    
+    const stack = [0];
+    for(let i=1;i<n;i++){
+        while(stack.length >0 && prices[i] < prices[stack[stack.length-1]]){
+            const j = stack.pop();
+            result[j] = i - j;
         }
+        stack.push(i);
     }
-    return stack;
+    
+    while(stack.length > 0){
+        const popped = stack.pop();
+        result[popped] = n - 1 - popped;
+    }
+    return result;
 }
