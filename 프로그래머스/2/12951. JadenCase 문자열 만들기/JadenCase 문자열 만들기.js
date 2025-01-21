abeb4052815227083c822f6ba492db8f7cc3cb14
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-21 09:41:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    const splitted = s.split(' ');
-    return splitted.map(str => str.charAt(0).toUpperCase()+str.slice(1).toLowerCase()).join(' ');
+    return s.split(' ').map(str => str.charAt(0).toUpperCase()+str.slice(1).toLowerCase()).join(' ');
 
 }
