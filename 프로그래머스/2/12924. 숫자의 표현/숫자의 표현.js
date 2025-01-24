78a098db0e08939ace7fc0545a8ebe5d0485ac63
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-01-24 10:55:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.30 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,14 +1,9 @@
 function solution(n) {
-    let count = 0;
-    
-    // k는 연속된 숫자의 개수
-    // (2 * n)의 제곱근까지만 확인하면 됨
-    for(let k = 1; k <= Math.sqrt(2 * n); k++) {
-        // n - (k * (k-1) / 2)가 k로 나누어 떨어지는지 확인
-        if((n - (k * (k-1) / 2)) % k === 0) {
-            count++;
-        }
-    }
-    
-    return count;
+    let answer = 0;
+  
+  for(let i = 0; i <= n; i++) {
+  	if(n%i === 0 && i%2 === 1) answer++;
+  }
+  
+  return answer;
 }
