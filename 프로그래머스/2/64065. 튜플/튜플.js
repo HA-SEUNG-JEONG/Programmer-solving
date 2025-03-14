680606e6e4e427e9b66e8fd401df83425c47feb5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-14 16:12:47 +0900

    [level 2] Title: 튜플, Time: 19.40 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -4,14 +4,14 @@ function solution(s) {
     // 중괄호 제거
     const arr = s.slice(2,-2).split('},{');
     const sets = arr.map(el => el.split(',').map(Number));
-    
     sets.sort((a,b) => a.length - b.length);
-    const result = [];
+    
+    const result = new Set();
     sets.forEach(set => {
         set.forEach(num => {
-            if(!result.includes(num)) result.push(num)
+            result.add(num)
         })
     });
-    return result;
+    return Array.from(result);
     
 }
