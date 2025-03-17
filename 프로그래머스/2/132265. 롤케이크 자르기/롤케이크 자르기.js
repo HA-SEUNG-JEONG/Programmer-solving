a32c3b2078293581bb11ce4af3e2e81d8eefa61f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-17 12:00:18 +0900

    [level 2] Title: 롤케이크 자르기, Time: 126.31 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,28 +1,30 @@
 function solution(topping) {
+    const rightCount = {};
+    let rightKinds = 0;
+    let leftKinds = 0;
     let result = 0;
-    const rightCount = new Map(); // 오른쪽 토핑 개수
-    const leftSet = new Set(); // 왼쪽 토핑 종류
-    let rightKinds = 0; // 오른쪽 토핑 종류 수
 
-    // 초기 오른쪽 토핑 상태 설정
     for (const t of topping) {
-        rightCount.set(t, (rightCount.get(t) || 0) + 1);
-    }
-    rightKinds = rightCount.size;
-
-    // 토핑을 하나씩 왼쪽으로 이동
-    for (const t of topping) {
-        // 왼쪽에 토핑 추가
-        leftSet.add(t);
-
-        // 오른쪽 토핑 개수 감소
-        rightCount.set(t, rightCount.get(t) - 1);
-        if (rightCount.get(t) === 0) {
-            rightKinds--;
+        if (!rightCount[t]) {
+            rightCount[t] = 0;
+            rightKinds++;
         }
+        rightCount[t]++;
+    }
 
-        // 양쪽의 토핑 종류 수가 같으면 결과 증가
-        if (leftSet.size === rightKinds) result++;
+    const leftCount = {};
+    
+    for (const t of topping) {
+        if (!leftCount[t]) {
+            leftCount[t] = 0;
+            leftKinds++;
+        }
+        leftCount[t]++;
+
+        rightCount[t]--;
+        if (rightCount[t] === 0) rightKinds--;
+
+        if (leftKinds === rightKinds) result++;
     }
 
     return result;
