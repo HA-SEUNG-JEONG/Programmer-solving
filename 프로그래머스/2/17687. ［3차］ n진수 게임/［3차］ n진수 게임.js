46986f75c62465d91d035398b94f012c9aaf2b14
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-04-24 10:56:21 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.14 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,18 +1,16 @@
 function solution(n, t, m, p) {
-  let answer = "";
-  let str = "";
-  let num = 0;
-
-  // 필요한 길이보다 충분히 긴 문자열 생성
-  while (str.length < t * m) {
-    str += num.toString(n).toUpperCase();
-    num++;
-  }
-
-  // 튜브의 순서에 맞는 문자만 선택
-  for (let i = 0; i < t; i++) {
-    answer += str[i * m + (p - 1)];
-  }
-
-  return answer;
+    let res = ''
+    let num = 0
+    let seq = ''
+    while (res.length < t) {
+        if (seq.length >= m) {
+            res += seq[p - 1]
+            seq = seq.slice(m)
+        }
+        else {
+            seq += num.toString(n).toUpperCase()
+            num++
+        }
+    }
+    return res
 }
