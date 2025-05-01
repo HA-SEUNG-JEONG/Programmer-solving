e151a154142a5664d407a26fc749f56fddab5d84
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-05-01 14:16:58 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 150.79 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const n = numbers.length;
+    const arr = new Array(n).fill(-1);
+    const stack = [];
+    
+    for(let i=0;i<n;i++){
+        while(stack.length > 0 && numbers[stack.at(-1)] < numbers[i]) {
+            arr[stack.pop()] = numbers[i];
+        }
+        stack.push(i)
+    }
+    return arr;
+}
