a20a8c34b00a85243ecf4649eead13e7b8f23791
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-05-29 09:59:59 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.45 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,17 +1,23 @@
 function solution(n) {
-    if (n === 1) return 1;
-    if (n === 2) return 2;
+//     if (n === 1) return 1;
+//     if (n === 2) return 2;
     
-    const MOD = 1000000007n;
-    let prev1 = 2n;  // dp[i-1]
-    let prev2 = 1n;  // dp[i-2]
-    let current;
+//     const MOD = 1000000007n;
+//     let prev1 = 2n;  // dp[i-1]
+//     let prev2 = 1n;  // dp[i-2]
+//     let current;
     
-    for (let i = 3; i <= n; i++) {
-        current = (prev1 + prev2) % MOD;
-        prev2 = prev1;
-        prev1 = current;
-    }
+//     for (let i = 3; i <= n; i++) {
+//         current = (prev1 + prev2) % MOD;
+//         prev2 = prev1;
+//         prev1 = current;
+//     }
     
-    return Number(current);
+//     return Number(current);
+    const arr = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000007;
+  }
+
+  return arr[n];
 }
