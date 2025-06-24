534316e619482062d010c8dfc2b85ceb59e10f97
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-24 21:16:44 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.82 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -12,7 +12,6 @@ function solution(s) {
         zeroCount += s.length - oneCount;
         // 2. s의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
         s = oneCount.toString(2);
-
         count++;
     }
     return [count, zeroCount];
