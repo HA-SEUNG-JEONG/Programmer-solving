44aa666b67988140f847354422042d917260567c
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-24 21:16:04 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.18 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -5,11 +5,13 @@ function solution(s) {
     // s가 '1'이 될때까지 s에 이진변환 가하기
     while (s !== "1") {
         // 1. s의 모든 0 제거하기
-        const removedZero = s.replace(/0/g, "");
-        zeroCount += s.length - removedZero.length;
-
+        let oneCount = 0;
+        for(const char of s){
+            if(char === '1') oneCount++;
+        }
+        zeroCount += s.length - oneCount;
         // 2. s의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
-        s = removedZero.length.toString(2);
+        s = oneCount.toString(2);
 
         count++;
     }
