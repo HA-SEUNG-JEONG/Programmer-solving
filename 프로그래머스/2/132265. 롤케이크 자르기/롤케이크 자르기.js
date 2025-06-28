38b675a4d3117e3101c2e3fc4eee0372c11b453e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-28 22:09:52 +0900

    [level 2] Title: 롤케이크 자르기, Time: 165.72 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,35 +1,38 @@
 function solution(topping) {
-    // let result = 0;
-    // // topping 배열을 자르기
-    // // (중복을 제외한) 배열에서 토핑 종류가 같으면 result 증가
-    // // 아니면 result 증가하지 않음
-    // for (let i = 0; i < topping.length; i++) {
-    //     const left = topping.slice(0, i);
-    //     const right = topping.slice(i);
-    //     const leftSet = new Set(left);
-    //     const rightSet = new Set(right);
-    //     if (leftSet.size === rightSet.size) result++;
-    // }
-    // return result;
+    const leftCount = {};
+    const rightCount = {};
+    let leftUnique = 0;
+    let rightUnique = 0;
+    let result = 0;
 
-    // 2. 슬라이딩 윈도우 기법 사용하기
-    const leftTopping = new Map();
-    const rightTopping = new Map();
-
-    for (let i = 0; i < topping.length; i++) {
-        rightTopping.set(topping[i], (rightTopping.get(topping[i]) || 0) + 1);
+    // 오른쪽 맵 초기화
+    for (const t of topping) {
+        if (!rightCount[t]) {
+            rightCount[t] = 0;
+            rightUnique++;
+        }
+        rightCount[t]++;
     }
 
-    let result = 0;
-    for (let i = 0; i < topping.length; i++) {
-        leftTopping.set(topping[i], (leftTopping.get(topping[i]) || 0) + 1);
-        rightTopping.set(topping[i], (rightTopping.get(topping[i]) || 0) - 1);
-        if (rightTopping.get(topping[i]) === 0) {
-            rightTopping.delete(topping[i]);
+    // 왼쪽으로 토핑 이동
+    for (const t of topping) {
+        // 왼쪽에 토핑 추가
+        if (!leftCount[t]) {
+            leftCount[t] = 0;
+            leftUnique++;
         }
-        if (leftTopping.size === rightTopping.size) {
+        leftCount[t]++;
+
+        // 오른쪽에서 토핑 제거
+        rightCount[t]--;
+        if (rightCount[t] === 0) {
+            rightUnique--;
+        }
+        // 비교
+        if (leftUnique === rightUnique) {
             result++;
         }
     }
+
     return result;
 }
