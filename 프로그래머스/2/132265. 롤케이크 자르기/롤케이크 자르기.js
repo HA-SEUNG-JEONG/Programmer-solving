233ca8cbedd1a8f0b5c83b8abf0f982c663092cb
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-06-28 22:07:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 146.09 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,31 +1,35 @@
 function solution(topping) {
-    const rightCount = {};
-    let rightKinds = 0;
-    let leftKinds = 0;
+    // let result = 0;
+    // // topping 배열을 자르기
+    // // (중복을 제외한) 배열에서 토핑 종류가 같으면 result 증가
+    // // 아니면 result 증가하지 않음
+    // for (let i = 0; i < topping.length; i++) {
+    //     const left = topping.slice(0, i);
+    //     const right = topping.slice(i);
+    //     const leftSet = new Set(left);
+    //     const rightSet = new Set(right);
+    //     if (leftSet.size === rightSet.size) result++;
+    // }
+    // return result;
+
+    // 2. 슬라이딩 윈도우 기법 사용하기
+    const leftTopping = new Map();
+    const rightTopping = new Map();
+
+    for (let i = 0; i < topping.length; i++) {
+        rightTopping.set(topping[i], (rightTopping.get(topping[i]) || 0) + 1);
+    }
+
     let result = 0;
-
-    for (const t of topping) {
-        if (!rightCount[t]) {
-            rightCount[t] = 0;
-            rightKinds++;
+    for (let i = 0; i < topping.length; i++) {
+        leftTopping.set(topping[i], (leftTopping.get(topping[i]) || 0) + 1);
+        rightTopping.set(topping[i], (rightTopping.get(topping[i]) || 0) - 1);
+        if (rightTopping.get(topping[i]) === 0) {
+            rightTopping.delete(topping[i]);
         }
-        rightCount[t]++;
-    }
-
-    const leftCount = {};
-    
-    for (const t of topping) {
-        if (!leftCount[t]) {
-            leftCount[t] = 0;
-            leftKinds++;
+        if (leftTopping.size === rightTopping.size) {
+            result++;
         }
-        leftCount[t]++;
-
-        rightCount[t]--;
-        if (rightCount[t] === 0) rightKinds--;
-
-        if (leftKinds === rightKinds) result++;
     }
-
     return result;
 }
