bfbc91f6862e88b62cc00a6e1b8161cce8a20b8f
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-07-07 08:45:40 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,7 +1,10 @@
 function solution(n)
 {
-    const binary = n.toString(2);
-    const oneCount = binary.split("1").length - 1;
+//     const binary = n.toString(2);
+//     const oneCount = binary.split("1").length - 1;
 
-    return oneCount;
+//     return oneCount;
+    
+    // 다른 답
+    return n.toString(2).split('').filter(el => el === '1').length;
 }
