bbbb300b5cd8a76c89e76d4e7393d671446ec14b
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-07-30 13:33:44 +0900

    [level 2] Title: 디펜스 게임, Time: 222.37 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -1,91 +1,79 @@
-class Heap {
+class MinHeap {
     constructor() {
         this.heap = [ null ]; 
     }
     
     size() {
-        return this.heap.length - 1;
+        return this.heap.length;
     }
     
-    getMax() {
-        return this.heap[1] ? this.heap[1] : null;
+    swap(idx1, idx2) {
+        [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
     }
     
-    swap(a, b) {
-        [ this.heap[a], this.heap[b] ] = [ this.heap[b], this.heap[a] ];
-    }
-    
-    push(value) {
+    add(value) {
         this.heap.push(value);
-        let curIdx = this.heap.length - 1;
-        let parIdx = (curIdx / 2) >> 0;
+        this.bubbleUp();
+    }
+    
+    poll(){
+        if(this.heap.length === 0) return null;
+        if(this.heap.length === 1) return this.heap.pop();
         
-        // 부모가 노드가 제일 작아야 하므로, 부모노드가 현재노드보다 큰 지  반복하여 체크한다.
-        while(curIdx > 1 && this.heap[parIdx] < this.heap[curIdx]) {
-        	// 구조분해 할당을 이용해 부모와 자식을 swap 한다.
-            this.swap(parIdx, curIdx)
-            curIdx = parIdx;
-            parIdx = (curIdx / 2) >> 0;
+        const minValue = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return minValue;
+    }
+    
+    bubbleUp() {
+        let currentIndex = this.heap.length - 1;
+        while(currentIndex > 0) {
+            const parentIndex = Math.floor((currentIndex - 1) / 2);
+            
+            if(this.heap[parentIndex] <= this.heap[currentIndex]) break;
+            this.swap(currentIndex,parentIndex);
+            currentIndex = parentIndex;
         }
     }
     
-    pop() {
-        // 배열 첫 원소를 비워두므로 root는 heap[1]에 항상 위치한다.
-        const max = this.heap[1];	
-        
-        /*  
-            배열 마지막 원소를 root 위치에 배치 과정.
-            if-else로 분기되는 이유는 추후 heap이 비었는지 아닌지 확인하기 위해 
-            size 체크 함수를 만들때 -1을 통해 0을 만들어주기 때문.
-        */
-        if(this.heap.length <= 2) this.heap = [ null ];
-        else this.heap[1] = this.heap.pop();   
-        
-        
-        let curIdx = 1;
-        let leftIdx = curIdx * 2;
-        let rightIdx = curIdx * 2 + 1; 
-        
-        if(!this.heap[leftIdx]) return max;
-         // 왼쪽 자식이 없다는 것은 오른쪽 자식도 없는, 즉 루트만 있는 상태이므로 바로 반환!
-        if(!this.heap[rightIdx]) {
-            if(this.heap[leftIdx] > this.heap[curIdx]) {
-				// 오른쪽 자식이 없다면 왼쪽 자식하나만 있다는 것을 의미한다.
-                this.swap(leftIdx, curIdx);
+    bubbleDown(){
+        let index = 0;
+        while(true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let smallestChildIndex = index;
+            
+            if(leftChildIndex <= this.heap.length && this.heap[leftChildIndex] < this.heap[smallestChildIndex]){
+                smallestChildIndex = leftChildIndex;
             }
-            return max;
+            
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
+                    smallestChildIndex = rightChildIndex;             
+            }
+            
+            if(smallestChildIndex === index) break;
+            
+            this.swap(index, smallestChildIndex);
+            index = smallestChildIndex;
         }
-
-       // 위에 조건에 걸리지 않는 경우 왼쪽과 오른쪽 자식이 모두 있는 경우이다.
-       // 따라서 현재 노드가 왼쪽 또는 오른쪽 보다 큰 지 작은지를 검사하며 반복한다.
-        while(this.heap[leftIdx] > this.heap[curIdx] || this.heap[rightIdx] > this.heap[curIdx]) {
-            // 왼쪽과 오른쪽 자식 중에 더 작은 값과 현재 노드를 교체하면 된다.
-            const maxIdx = this.heap[leftIdx] < this.heap[rightIdx] ? rightIdx : leftIdx;
-            this.swap(maxIdx, curIdx);
-            curIdx = maxIdx;
-            leftIdx = curIdx * 2;
-            rightIdx = curIdx * 2 + 1;
-        }
-
-        return max;
     }
 }
 
+
 function solution(n, k, enemy) {
     
-    let heap = new Heap()
-    let ret = 0
-    for (let i = 0; i < enemy.length ; i++){
+    const minHeap = new MinHeap();
+    for(let i=0; i<enemy.length;i++){
+        const currentEnemy = enemy[i];
+        minHeap.add(currentEnemy);
         
-        heap.push(enemy[i])
-        n -= enemy[i];
-        if (n < 0 ) {
-            if(k===0 || n+heap.getMax() < 0 ) return ret
-            k --
-            n += heap.pop()
+        if(minHeap.size() > k){
+            const weakestEnemy = minHeap.poll();
+            n -= weakestEnemy;
         }
-        ret +=1
+        
+        if(n < 0) return i;
     }
-    return ret
-    
+    return enemy.length;
 }
