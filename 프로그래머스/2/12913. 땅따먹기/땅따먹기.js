b9f33e0a263bfa4b88838412f662bcaa398fe3b5
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-08-18 20:44:24 +0900

    [level 2] Title: 땅따먹기, Time: 52.11 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -6,5 +6,5 @@ function solution(land) {
             land[i][j] += Math.max(...land[i-1].filter((_,i) => i !== j))
         }
     }
-    return Math.max(...land.at(-1))
+    return Math.max(...land[land.length-1])
 }
