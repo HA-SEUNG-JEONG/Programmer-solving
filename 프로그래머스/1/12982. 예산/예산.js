51ae1eaffbf268543cd29e37133274d5385f0c98
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-10-29 17:13:50 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,19 +1,10 @@
 function solution(d, budget) {
-    // d : 부서별로 신청한 금액이 들어있는 배열 
-    // budget : 예산
-    // 최대 몇 개의 부서에 물품을 지원할 수 있는지 return
-    
+    d.sort((a,b) => a - b);
     let result = 0;
-    d.sort((a,b) => a-b);
-    
-    // d 배열 중에서 n개를 골라서 그 합이 budget보다 작아야 함
-    
-    for(let amount of d){
-        if(budget >= amount){
-            budget -= amount;
+    for(let i=0; i<d.length;i++){
+        if(budget >= d[i]){
+            budget -= d[i];
             result++;
-        }else{
-            break;
         }
     }
     return result;
