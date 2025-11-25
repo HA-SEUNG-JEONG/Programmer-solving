d24605baa083a3de80d436a0e41766bc0bd72f50
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-11-25 19:27:23 +0900

    [level 2] Title: 구명보트, Time: 31.72 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,27 +1,20 @@
 function solution(people, limit) {
-    // 최대 2명, 1명만 타도 됨
-    // let count = 0;
-    // for(let i=0;i<people.length;i++){
-    //     for(let j=i;j<people.length;j++){
-    //         if(people[i] > limit || people[j] > limit) return
-    //         else if(people[i] <= limit || people[j] <= limit) count++;
-    //         else if(people[i] + people[j] <= limit){
-    //             count++;
-    //         }
-    //     }
-    // }
-    // return count;
-    people.sort((a,b) => a-b);
-    let count = 0;
-    let left = 0;
-    let right = people.length - 1;
-    
-    while(left <= right){
-        if(people[left] + people[right] <= limit){
-            left++;
-        }
-        right--;
-        count++;
+  people.sort((a, b) => a - b);
+
+  let left = 0;
+  let right = people.length - 1;
+
+  let boats = 0;
+
+  while (left <= right) {
+    if (people[left] + people[right] <= limit) {
+      left++;
+      right--;
+      boats++;
+    } else {
+      right--;
+      boats++;
     }
-    return count;
+  }
+  return boats;
 }
